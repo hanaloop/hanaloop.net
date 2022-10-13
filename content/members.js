b9ed42aa0cd2f469f825db7b864304b9b69c6aa3
김혜연 <hykim@hanaloop.com>
2022-10-13 08:57:15 +0000

    Update content/members.js

--- content/members.js
+++ content/members.js
@@ -6,7 +6,7 @@ module.exports = {
     "image_url": "/images/who-we-are/hykim-20210704.jpg",
     "title": "co-CEO / 지속가능성 연구원",
     "tagline": "지구환경을 회복시키기 위해 인력 네트워크를 만들려는",
-    "education": "고려대 석사",
+    "education": "고려대 석사, 이화여대 학사",
     "description": "삼성전자, Altenia, Arteplus, KOWORC 등. 프로젝트: 음성인식기 최적화, 모바일 앱 개발, NIA 해외 과학기술 동향 조사 등. 온실가스관리기사. LEED Green Associate. 임베디드 소프트웨어 개발. 비영리 조직 운영. 산업 및 정책에 대한 연구 수행.  AI 논문 저자."
   },
   "ysahnpark": {
@@ -16,7 +16,7 @@ module.exports = {
     "image_url": "/images/who-we-are/ysahnpark-20130815.jpg",
     "title": "co-CEO / CTO",
     "tagline": "아름다운 지구환경을 딸에게 물려주려는",
-    "education": "Carnegie Mellon University 석사",
+    "education": "Carnegie Mellon University 석사, 고려대 학사",
     "description": "Walt Disney, Pearson, Wayfair, Altenia 등. 프로젝트: 이마트 DT, 디즈니 미디어 플랫폼, LG 콜센터, 신공항고속도로 통합, 한국통신 MMS 등 20년 이상 IT 업계에서 팀과 조직을 성공적으로 이끌며 대규모 엔터프라이즈 어플리케이션 개발. 보안 및 생물정보학 논문 저자."
   },
   "hkroh": {
@@ -25,8 +25,8 @@ module.exports = {
     "image_url": "/images/who-we-are/kay.jpeg",
     "title": "수석엔지니어",
     "tagline": "기술로 의미있는 일을 하고싶은",
-    "education": "고려대 석사",
-    "description": "다수의 상용 소프트웨어 개발 및 AI 프로젝트 수행. 멀티미디어, 모바일 앱, 웹 플랫폼, 머신 러닝 프로그램 다수 개발"
+    "education": "고려대 석사, 학사",
+    "description": "아이큐브, 아프로텍, 지능정보기술연구원 등. 프로젝트: 다수의 상용 소프트웨어 개발 및 AI 프로젝트 수행. 멀티미디어, 모바일 앱, 웹 플랫폼, 머신 러닝 프로그램 다수 개발"
   },
   "hana": {
     "type": "system",
