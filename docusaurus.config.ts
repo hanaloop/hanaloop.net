038456073f10e48832c8f08d8b6ba98b65b26d3d
jeongdopark <jeongdo.dev@gmail.com>
2025-02-20 17:12:49 +0900

    CBAM URL 변경된 포스팅 구URL에서 신URL로 리다이렉트

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -192,7 +192,20 @@ const config: Config = {
           {
             from: '/hanaAI',
             to: '/hana-ai'
+          },
+          {
+            from: '/docs/x20_environment-general/2023-09-28-CBAM-reporting',
+            to: '.docs/x20_environment-general/CBAM/2023-09-28-CBAM-reporting'
+          },
+          {
+            from: '/docs/x20_environment-general/2022-12-14-CBAM',
+            to: '/docs/x20_environment-general/CBAM/2022-12-14-CBAM'
+          },
+          {
+            from: '/docs/x20_environment-general/2024-01-20-cbam-compliance',
+            to: '/docs/x20_environment-general/CBAM/2024-01-20-cbam-compliance'
           }
+
         ],
         createRedirects: path => {
           if (path === "/hanaAI") {
