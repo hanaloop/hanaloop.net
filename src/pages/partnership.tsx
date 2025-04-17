e8dcccf3e6a1f7661de6c5a72f95a56e87f0fa96
jeongdopark <jeongdo.dev@gmail.com>
2025-04-17 16:25:43 +0900

    renewal partnership page

--- src/pages/partnership.tsx
+++ src/pages/partnership.tsx
@@ -7,6 +7,7 @@ import Hero from '../components/hanaloop/Hero';
 import SectionBlock from "../components/theme/SectionBlock";
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { motion } from "framer-motion";
 
 export default function Home(): JSX.Element {
   const {siteConfig, i18n} = useDocusaurusContext();
@@ -28,34 +29,51 @@ export default function Home(): JSX.Element {
         }
       />
 
-      <SectionBlock title={data.content.title} >
-        <div className="m-5 border rounded-lg grid grid-cols-1  md:grid-cols-2 gap-2">
-        { data.content.items.map((item, ndx) =>
-          <div key={ndx} className="p-4 group hover:bg-slate-100">
-            <div className="text-lg font-bold group-hover:text-primary-700">{item.title}</div>
-            <article>{item.description}</article>
-          </div>
-        )}
-        </div>
-      </SectionBlock>
-
-      <SectionBlock title={data.alliances.title} containerStyle='bg-gray-100 flex justify-center'>
-        <article className="text-left">
-        <ul>
-          {data.alliances.items.map((item, idx) => 
-            <li key={idx}>
-              {item.sourceUrl ? <a href={item.sourceUrl}>{item.title}</a> : item.title}
-            </li>
-          )}
-        </ul>
-        </article>
-      </SectionBlock>
-
-
-      <div className="flex justify-center" >
-        <img src={useBaseUrl("/images/banners/pyeongchanggun-windfarm.jpeg")} />
+      <div className="w-screen flex justify-center bg-gray-50">
+        <motion.div
+          viewport={{ once: true }}
+          initial={{ opacity: 0, y: 60 }} 
+          whileInView={{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1 },
+          }}
+        >
+          <SectionBlock title={data.content.title}>
+            <div className="px-10 w-full flex justify-center">
+              <div className="w-full flex flex-col gap-10 items-center">
+                {data.content.items.map((item, ndx) => {
+                  // const lines = item.description as string[];
+                  return (
+                    <div
+                      className="group w-full rounded-xl hover:border-primary-700 mb-4 py-5 bg-white shadow-sm flex justify-center"
+                      key={ndx}
+                    >
+                      <div className="mb-2 flex flex-col justify-start items-start text-left gap-3 h-full w-[90%]">
+                        <div className='flex lg:flex-row flex-col-reverse items-center gap-4'>
+                          <div className='flex flex-col items-start'>
+                            <div className="xl:text-xl text-lg font-bold">
+                              <div className="flex mr-1 items-baseline group-hover:text-primary-700 justify-center">
+                                <h3 className="py-2 inline">{item.title}</h3>
+                              </div>
+                            </div>
+                            <div className="[word-break:keep-all] w-full">
+                              <h4 className="pb-2 text-md font-semibold text-gray-500 text-left">{item.description}</h4>
+                            </div>
+                          </div>
+                          <div className='w-[200px] h-[200px] flex-shrink-0 rounded-md flex justify-center items-center'>
+                            <img src={`/images/partnership_${ndx+1}.png`} height="200px" width="200px" className='rouneded-md' />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </SectionBlock>
+        </motion.div>
       </div>
-
     </Layout>
   );
 }
