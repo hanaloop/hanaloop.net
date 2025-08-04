118a864adff69c2d5d851716c75321c929c688f2
jeongdopark <jeongdo.dev@gmail.com>
2025-08-04 11:09:20 +0900

    이슈리포트 링크 추가 및 주소 변경

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -6,7 +6,7 @@ export default function FooterWrapper(props) {
     <>
       <Footer {...props} />
       <div className='py-2 bg-[#303846] text-white text-center'>
-        <div>서울특별시 강남구 선릉로93길 40, 나라키움 역삼A빌딩 4층</div>
+        <div>서울특별시 관악구 봉천로 545, 서울창업센터 관악 4층</div>
         <div>email: info@hanaloop.com  |  phone: +82 0507-1337-9251</div>
       </div>
 
