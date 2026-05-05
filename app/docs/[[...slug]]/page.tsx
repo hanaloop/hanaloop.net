9899d33583585a9e4bc9fdb9aaddfb848f3c151a
BY Kim <logaegae@gmail.com>
2026-05-06 00:33:52 +0900

    누락 코드 추가

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,5 +1,11 @@
 import { UnderConstruction } from '@/components/features/under-construction';
 
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return [{ slug: [] }];
+}
+
 export default function Page() {
   return <UnderConstruction />;
 }
