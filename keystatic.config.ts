fae18ce86053f0930cac80c1e6240d2af5f9771c
Zero Hnlp <ys.ahnpark@hanaloop.com>
2026-06-01 17:37:21 +0900

    Working config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,19 +1,45 @@
 // keystatic.config.ts
 import { config, fields, collection } from '@keystatic/core';
+import { createElement } from 'react';
 
 export default config({
   storage: {
     kind: 'local',
   },
   collections: {
-    posts: collection({
-      label: 'Posts',
+    blogs: collection({
+      label: 'Blog',
       slugField: 'title',
-      path: 'src/content/ko/blog/*',
+      path: 'content/en/blog/**',
       format: { contentField: 'content' },
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({ label: 'Content' }),
+        date: fields.date({ label: 'Published date' }),
+        subtitle: fields.text({ label: 'Subtitle' }),
+        description: fields.text({ label: 'Description' }),
+        summary: fields.text({ label: 'Summary' }),
+        image: fields.image({ label: 'Image' }),
+        authors: fields.array(fields.text({ label: 'Author' }), { label: 'Authors' }),
+        tags: fields.array(fields.text({ label: 'Tag' }), { label: 'Tags' }),
+
+        content: fields.mdx({ 
+            label: 'Content',
+            components: {
+                CaptionedImage: {
+                    kind: 'block',
+                    icon: createElement('div', null, 'Image'),
+                    label: 'Captioned Image',
+                    schema: {
+                        imageSrc: fields.text({ label: 'Image URL' }),
+                        alt: fields.text({ label: 'Alt text' }),
+                        containerClassName: fields.text({ label: 'Classname' }),
+                        imageClassName: fields.text({ label: 'Classname' }),
+                        caption: fields.text({ label: 'Caption' }),
+                    },
+                },
+            }
+        }),
+
       },
     }),
   },
